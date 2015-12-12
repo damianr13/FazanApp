95bea329d4a5d7a2c08f54578affdcaa81887205
Bogdan <bogdan.talpiga@advahoo.com>
2015-12-12 13:01:10 +0200

    public

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,27 @@
-'use strict'
+'use strict';
 
 var app=angular.module('FazanApp',[]);
 
 app.controller('FazanController',['$scope','$http',function($scope,$http) {
+var show='pg1';
+$scope.show=show;
+var connectTo='http://'+document.domain+":"+location.port;
+var socketClient=io.connect()
+//$scope.userName=userName;
+var user={"name":$scope.userName}
 
- var user={"name":userName}
-$scope.userName=userName;
-
-$http.post(' ',user)
+$scope.register=function(gamePlay){
+	user['gamePlay']=gamePlay;
+	$http.post(' ',user)
       .then(function (data){
-
+      			$scope.show=gamePlay;
             }, 
 
     		function(data){
       			console.log("eroare");
+      			$scope.show=gamePlay;//TODO remove
             })
+
+}
+
 }])
