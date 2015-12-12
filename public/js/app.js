f900c261827bd7871dceb6fe60190ba35749515e
mario <ionescumarius23@gmail.com>
2015-12-12 11:50:06 +0200

    user json

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,18 @@
 
 var app=angular.module('FazanApp',[]);
 
-app.controller('FazanController',['$scope','$http'],
-	function($scope,$http))
+app.controller('FazanController',['$scope','$http',function($scope,$http) {
+
+ var user={"name":userName}
+$scope.userName=userName;
+
+$http.post(' ',user)
+      .then(function (data){
+
+                        }, 
+
+    function(data){
+      console.log("eroare");
+                  }
+           )
+}])
